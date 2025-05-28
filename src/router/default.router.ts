4cf5c5a45ea23a06d08c869693384b715f04a696
zlaam <zlaam.dev@gmail.com>
2025-05-28 05:36:03 +0500

    feat: Add project router

--- src/router/default.router.ts
+++ src/router/default.router.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { authRouter } from "./userRouter/auth.router";
 import { updateUserRouter } from "./userRouter/updateUser.router";
+import { projectRouter } from "./projectRouter/project.router";
 export const defaultRouter: Router = Router();
 
 // *** User
 defaultRouter.use("/user", authRouter);
 defaultRouter.use("/user", updateUserRouter);
+// *** Project
+defaultRouter.use("/project", projectRouter);
+// *** Code Container
+
+// *** Folder
+
+// *** Workspace
---
+++ src/router/projectRouter/project.router.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { authMiddleware } from "../../middleware/globalMiddleware/auth.middleware";
+import { database } from "../../db/db";
+import { projectController } from "../../controller/projectController/project.controller";
+
+export const projectRouter: Router = Router();
+
+projectRouter
+  .route("/createProject")
+  .post(
+    authMiddleware(database.db).checkToken,
+    projectController(database.db).createProject,
+  );
+projectRouter
+  .route("/getSingleProject")
+  .get(
+    authMiddleware(database.db).checkToken,
+    projectController(database.db).getSingleProject,
+  );
+projectRouter
+  .route("/getAllProjects")
+  .get(
+    authMiddleware(database.db).checkToken,
+    projectController(database.db).getAllProjects,
+  );
+projectRouter
+  .route("/updateProject")
+  .patch(
+    authMiddleware(database.db).checkToken,
+    projectController(database.db).updateProject,
+  );
+projectRouter
+  .route("/deleteProject")
+  .delete(
+    authMiddleware(database.db).checkToken,
+    projectController(database.db).deleteProject,
+  );
